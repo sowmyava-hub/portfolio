b71dddf803af4c7a35d01d5dbafead999a201f57
sowmyava-hub <123717995+sowmyava-hub@users.noreply.github.com>
2023-08-12 21:02:06 -0400

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import { ThemeContext } from './contexts/ThemeContext';
 import { Main, ProjectPage } from './pages'
